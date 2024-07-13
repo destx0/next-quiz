b7e4c46956e8519e84331433ec28066af59e0f31
destx0 <destisdbest@gmail.com>
2024-07-13 19:37:23 +0530

    removed previous

--- stores/quizStore.js
+++ stores/quizStore.js
@@ -161,30 +161,6 @@ const useQuizStore = create((set, get) => ({
 			return state; // No change if we're at the last question
 		}),
 
-	previousQuestion: () =>
-		set((state) => {
-			const { currentSectionIndex, currentQuestionIndex, sections } =
-				state.quizData;
-			if (currentQuestionIndex > 0) {
-				return {
-					quizData: {
-						...state.quizData,
-						currentQuestionIndex: currentQuestionIndex - 1,
-					},
-				};
-			} else if (currentSectionIndex > 0) {
-				const previousSection = sections[currentSectionIndex - 1];
-				return {
-					quizData: {
-						...state.quizData,
-						currentSectionIndex: currentSectionIndex - 1,
-						currentQuestionIndex:
-							previousSection.questions.length - 1,
-					},
-				};
-			}
-			return state; // No change if we're at the first question
-		}),
 	submitQuiz: () =>
 		set((state) => ({
 			isSubmitted: true,
