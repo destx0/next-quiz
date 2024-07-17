6ad9ecc518ff817d2f5148becefb9dc224587302
destx0 <destisdbest@gmail.com>
2024-07-17 09:44:02 +0530

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { Facebook, Twitter, Instagram, Linkedin, Mail } from "lucide-react";
 
 export default function Footer() {
 	return (
-		<footer className="bg-gray-100 dark:bg-gray-900 py-12">
+		<footer className=" py-12">
 			<div className="container mx-auto px-4">
 				<div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
 					<div>
