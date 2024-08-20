1794a25584f69c06d6378c4e1be599c82eacc006
destx0 <destisdbest@gmail.com>
2024-08-20 21:21:48 +0530

    show saubmiossion

--- app/ssc-mock/QuizCard.jsx
+++ app/ssc-mock/QuizCard.jsx
@@ -86,7 +86,7 @@ const QuizCard = ({ quiz, index }) => {
 								color="secondary"
 								className="transition-all duration-300 ease-in-out transform group-hover:scale-110 text-white font-semibold px-4 py-2 rounded-full bg-gradient-to-br from-green-500 to-teal-400 hover:from-green-600 hover:to-teal-500"
 							>
-								Show Previous Submission
+								Show Submission
 							</Button>
 						</Link>
 					)}
