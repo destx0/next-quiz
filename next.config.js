9c749a66903d958b499867df1f65e62562abf276
destx0 <destisdbest@gmail.com>
2024-10-05 20:20:30 +0530

    jjj

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      };
-    }
-    return config;
-  },
-  // If you're using the app directory
-  experimental: {
-    appDir: true,
-  },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
